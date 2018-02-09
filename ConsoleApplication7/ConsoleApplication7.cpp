483f5f8e5e8f4b76177fb7e2c2f07273c0abe533
Catherine-yaoyao <36300380+Catherine-yaoyao@users.noreply.github.com>
2018-02-09 21:21:38 +0800

    Update ConsoleApplication7.cpp

--- ConsoleApplication7/ConsoleApplication7.cpp
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -1,11 +1,69 @@
-// ConsoleApplication7.cpp : ¶¨Òå¿ØÖÆÌ¨Ó¦ÓÃ³ÌĞòµÄÈë¿Úµã¡£
+// ConsoleApplication7.cpp : å®šä¹‰æ§åˆ¶å°åº”ç”¨ç¨‹åºçš„å…¥å£ç‚¹ã€‚
 //
 
+
+
 #include "stdafx.h"
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#pragma warning(disable:4996)
 
 
-int main()
+
+int trimSpace(char *str, char *newstr) {
+	char *p = str;
+	int i, j;
+	int ncount;
+	i = 0;
+	j = strlen(p) - 1;
+	while (isspace(p[i]) && p[i] != '\0') {
+		i++;
+	}
+	while (isspace(p[j]) && p[j] != '\0') {
+		j--;
+	}
+	ncount = j - i + 1;
+	strncpy(newstr, p + i, ncount);
+	newstr[ncount] = '\0';
+	return 0;
+}
+
+int getKeyByValue(char *keyvaluebuf, char *keybuf,char *valuebuf) {
+	int ret = 0;
+	if (keyvaluebuf == NULL || keybuf == NULL || valuebuf == NULL) {
+		return ret;
+	}
+	char *p = keyvaluebuf;
+	p = strstr(keyvaluebuf, keybuf);
+	if (p == NULL) {
+		return -1;
+	}
+	p = p + strlen(keybuf);
+	p = strstr(keyvaluebuf, "=");
+	if (p == NULL) {
+		return -1;
+	}
+	p = p + strlen("=");
+	ret = trimSpace(p, valuebuf);
+	if (ret != 0) {
+		printf("the func trimSpace err:%d", ret);
+		return ret;
+	}
+	return ret;
+
+}
+void main()
 {
-    return 0;
+	int ret = 0;
+	char buf[1024] = { 0 };
+
+	char *keyandvalue = "key2=     value2   ";
+	char *key = "key2";
+	ret = getKeyByValue(keyandvalue, key, buf );
+	if (ret != '\0') {
+		printf("func getKeyByValue err:%d", ret);
+	}
+	printf(" the buf is %s", buf);
 }
 
